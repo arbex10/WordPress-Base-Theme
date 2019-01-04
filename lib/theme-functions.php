1564f44b51cb8fc116fbe2af4a489350929bca20
Lucas Arbex <lucasarbexmb@gmail.com>
2019-01-04 20:54:22 -0200

    Fix meta function

--- lib/theme-functions.php
+++ lib/theme-functions.php
@@ -2,7 +2,7 @@
 
 // Function to display meta
 function wbt_meta() {
-  echo '<h6>' get_the_author() . '   |   ' . get_the_date('d/m/Y') . ' - ' . get_the_time() . '</h6>';
+  echo '<h6>' . get_the_author() . '   |   ' . get_the_date('d/m/Y') . ' - ' . get_the_time() . '</h6>';
 }
 
 ?>
