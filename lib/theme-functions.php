56c672f891e9f2ea2a337e58376369bf30e792ec
Lucas Arbex <lucasarbexmb@gmail.com>
2019-01-04 23:41:41 -0200

    Add function of lenght excerpt

--- lib/theme-functions.php
+++ lib/theme-functions.php
@@ -5,4 +5,16 @@ function wbt_meta() {
   echo '<h6>' . get_the_author() . '   |   ' . get_the_date('d/m/Y') . ' - ' . get_the_time() . '</h6>';
 }
 
+// Function to limit excerpt lenght
+function get_excerpt($limit){
+	$excerpt = get_the_content();
+	$excerpt = preg_replace(" ([.*?])",'',$excerpt);
+	$excerpt = strip_shortcodes($excerpt);
+	$excerpt = strip_tags($excerpt);
+	$excerpt = substr($excerpt, 0, $limit);
+	$excerpt = substr($excerpt, 0, strripos($excerpt, " "));
+	$excerpt = trim(preg_replace( '/\s+/', ' ', $excerpt));
+	return $excerpt;
+}
+
 ?>
